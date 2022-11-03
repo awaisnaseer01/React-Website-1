0105dae578da49ef6530cd1f67c0dd3773915c74
awaisnaseer01 <116357708+awaisnaseer01@users.noreply.github.com>
2022-11-03 21:31:14 +0500

    React Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Components/Header'
+import Header from './Components/Header';
+import Navbar1 from './Common/Navbar1';
+import Navbar2 from './Common/Navbar2';
+import Navbar3 from './Common/Navbar3';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar1 />
+      <Navbar2 />
+      <Navbar3 />
     </div>
   );
 }
---
+++ src/Common/Navbar2.jsx
@@ -0,0 +1,77 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {BsFillPersonFill, BsHeart, BsBagCheck} from 'react-icons/bs';
+
+
+function Navbar2() {
+  return (
+<>
+    <div className='row'>
+
+    <Navbar  bg="light" expand="lg">
+      <Container fluid>
+
+        {/* <-------Bestore image-----> */}
+
+    <div className='col-1'>
+        <Navbar.Brand href="#"><img className='bestore_image' src='https://themes.muffingroup.com/betheme-store/wp-content/uploads/2021/09/retina-bethemestore.png' /></Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        
+        <Navbar.Collapse id="navbarScroll">
+            
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            
+          </Nav>
+
+           
+          {/* <-------Search Bar-------> */}
+          
+
+          <div className='col-9'>
+          <Form className="d-flex">
+            <input
+              type="search"
+              placeholder="Enter your search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button className='search_btn' variant="outline-success">Search</Button>
+          </Form>
+          </div>
+
+<div className='col-1'>
+          {/* <--------logos------> */}
+
+
+
+          <Nav className="me-auto">
+            <Nav.Link href="#home"><BsFillPersonFill/></Nav.Link>
+            <Nav.Link href="#features"><BsHeart /></Nav.Link>
+            <Nav.Link href="#pricing"><BsBagCheck/></Nav.Link>
+            
+          </Nav>
+
+          </div>
+
+          <div className='col-1'>
+
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    </div>
+
+    </>
+  );
+}
+
+export default Navbar2;
